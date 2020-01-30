130dcd86620482279b27535ee52bdbb33851d8fa
christiangoncalves <christiangoncalves19@gmail.com>
2020-01-30 10:31:05 -0300

    correcao de pastas

---
+++ react-tracking_/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import TrackingEvents from './components/TrackingEvents/index';
+import TrackingForm from './components/TrackingForm/index';
+
+import {track} from './helpers/ApiHelper';
+
+function App() {
+
+  const [events, setEvents] = useState([]);
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    track(data.tracking).then(setEvents).catch(console.error);
+
+  }
+
+  return (
+    <div className="container">
+      <TrackingForm submitHandler={submitHandler}/>
+      <TrackingEvents events={events} />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-tracking_/src/components/TrackingEvents/index.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+
+function TrackingEvents({ events }) {
+  if (!events || events.length === 0) return null;
+
+  return (
+    <Fragment>
+      <h1>Tracking Events</h1>
+      <ul className="list-group" >
+        {events.map(item => {
+          return (
+            <li key={item.descricao} className="list-group-item">{item.descricao}
+              <span> {item.data} {item.hora} </span>
+              <span>{item.cidade}/{item.uf}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default TrackingEvents;
